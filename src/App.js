e5bd70035cf6b35f644bce3c08da93e8875de637
Kostya <forgit266@gmail.com>
2023-07-19 16:24:03 +0000

    remarks 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import Calendar from "./components/Calendar";
-import { startOfMonth, startOfWeek } from "date-fns";
-import { LocaleDateContext } from "./contexts";
 
 export default function App() {
-  const currentDate = new Date();
-  const firstDayOfFirstWeek = startOfWeek(startOfMonth(currentDate));
-
-  return (
-    <LocaleDateContext.Provider value={{ currentDate, firstDayOfFirstWeek }}>
-      <Calendar />;
-    </LocaleDateContext.Provider>
-  );
+  return <Calendar />;
 }
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,13 +1,21 @@
 import Month from "../Month";
 import Today from "../Today";
 import style from "./Calendar.module.scss";
+import { startOfMonth, startOfWeek } from "date-fns";
+import { LocaleDateContext } from "../../contexts";
 
 export default function Calendar() {
+  const currentDate = new Date();
+  const firstDayOfFirstWeek = startOfWeek(startOfMonth(currentDate));
+
   return (
-    <div className={style.calendar}>
-      <Today />
-      <Month /> {/*технически высвобождает место для Today */}
-      <Month />
-    </div>
+    <LocaleDateContext.Provider value={{ currentDate, firstDayOfFirstWeek }}>
+      <div className={style.calendar}>
+        <Today />
+        <Month />
+
+        <Month />
+      </div>
+    </LocaleDateContext.Provider>
   );
 }
